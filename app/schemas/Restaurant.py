55b9ff0de8f4acf06aeec027dd5969a011b2cb58
JulianMohamed12 <julianmmohamed@gmail.com>
2026-02-27 14:26:24 -0800

    added Restaurant schema. Will add depenedency on the RestaurantMenu schema later

---
+++ app/schemas/Restaurant.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+
+class Restaurant(BaseModel):
+    id: str
+    owner_id: str
+    name: str
+    location: str
+    postal_code: str
+    delivery_radius: float
+    is_active: bool
+class RestaurantCreate(BaseModel):
+    owner_id: str
+    name: str
+    location: str
+    postal_code: str
+    delivery_radius: float
+class RestaurantUpdate(BaseModel):
+    owner_id: str
+    name: str
+    location: str
+    postal_code: str
+    delivery_radius: float
+    is_active: bool
