3e66bf238bea6a7c761c6d6750ee01c4a2e17ea0
BransonCr <bransonacrawford@gmail.com>
2026-03-02 15:09:27 -0800

    Update and add the Review schema

---
+++ app/schemas/Review.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+
+
+class Review(BaseModel):
+    review_id: str
+    customer_id: str
+    restaurant_id: str
+    order_id: str
+    rating: int
+    text: str
+    created_at: str
+
+
+class ReviewCreate(BaseModel):
+    customer_id: str
+    restaurant_id: str
+    order_id: str
+    rating: int
+    text: str
+
+
+class ReviewUpdate(BaseModel):
+    rating: int
+    text: str
+
+
+# update to be changed when needed.
