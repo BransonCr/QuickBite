701ac14674f55a21e0c9f09cfdb7d655acfb12a1
VDhillon05 <vinaydhillon2005@gmail.com>
2026-03-04 12:38:44 -0800

    Created Notification Schema

---
+++ app/schemas/Notification.py
@@ -0,0 +1,22 @@
+class Notification(BaseModel):
+    notification_id: str
+    user_id: str
+    order_id: str
+    message: str
+    type: str
+    is_read: bool
+    created_at: str
+
+class NotificationCreate(BaseModel):
+    user_id: str
+    order_id: str
+    message: str
+    type: str
+    is_read: bool
+
+class NotificationUpdate(BaseModel):
+    user_id: str
+    order_id: str
+    message: str
+    type: str
+    is_read: bool
