a05346f5dbb6232c4ee8ab0aeeb2aa21b9257300
JulianMohamed12 <julianmmohamed@gmail.com>
2026-03-05 16:37:41 -0800

    added notification routes and imported into main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 
+from app.routers import Notfication
+
 app = FastAPI(title="QuickBite", version="0.1.0")
+
+app.include_router(Notfication.router)
 
 
 @app.get("/health")
