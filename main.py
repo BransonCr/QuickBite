9564c354d48323464f20267f1dd2653880e2c273
Leo Arthur Cabral <109047018+Kingking11xD@users.noreply.github.com>
2026-03-05 22:47:42 -0800

    Include Payment router in FastAPI application

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
+
+from app.routers import Payment
 app = FastAPI(title="QuickBite", version="0.1.0")
 
+
+app.include_router(Payment.router)
 
 @app.get("/health")
 def health():
