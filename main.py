510aca6e514c2723a2f32cb14a9d124515bd1ac3
VDhillon05 <vinaydhillon2005@gmail.com>
2026-03-05 19:37:36 -0800

    Added Order Routes and Imported into main

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-
+from app.routers import Order
 app = FastAPI(title="QuickBite", version="0.1.0")
 
+app.include_router(Order.router)
 
 @app.get("/health")
 def health():
