3c3850a763895cf592d0e8051668ba9c85f36ec3
Leo Arthur Cabral <109047018+Kingking11xD@users.noreply.github.com>
2026-03-05 22:43:42 -0800

    Include Restaurant router in FastAPI app

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
+
+from app.routers import Restaurant
 app = FastAPI(title="QuickBite", version="0.1.0")
 
+
+app.include_router(Restuarant.router)
 
 @app.get("/health")
 def health():
