c22c457aa2bb2d99d84542a30f99a3c65363cc21
JulianMohamed12 <julianmmohamed@gmail.com>
2026-03-07 18:17:42 -0800

    Added MenuItem service and model

---
+++ app/models/MenuItemModel.py
@@ -0,0 +1,23 @@
+import csv
+from pathlib import Path
+from typing import List
+
+from app.schemas.MenuItem import MenuItem
+
+DATA_DIR = Path(__file__).parent.parent / "db"
+MENUITEMS_CSV = DATA_DIR / "users.csv"
+
+def load_all() -> List[MenuItem]:
+    menu_items = []
+    with open(MENUITEMS_CSV, "r") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            menu_items.append(MenuItem.model_validate(row))
+    return menu_items
+
+def save_all(menu_items: List[MenuItem]) -> None:
+    with open(MENUITEMS_CSV, "w") as f:
+        writer = csv.DictWriter(f, fieldnames=list(MenuItem.model_fields.keys()))
+        writer.writeheader()
+        for menu_item in menu_items:
+            writer.writerow(menu_items.model_dump())
---
+++ app/services/MenuItemService.py
@@ -0,0 +1,48 @@
+import uuid
+
+from fastapi import HTTPException
+
+from app.models.MenuItemModel import load_all, save_all
+from app.schemas.MenuItem import MenuItem, MenuItemCreate, MenuItemUpdate
+from app.services.MenuItemService import MenuItemService
+
+
+class MenuItemService:
+    def get_menu_items(self):
+        return load_all()
+
+    def create_menu_item(self, menu_item: MenuItemCreate) -> MenuItem:
+        # uuid generates a new item id.
+        # also note that menu_item.model_dump() returns a dict with all fields, however passing in
+        # menu_item.model_dump() directly as pos argument is not allowed, so we use kwargs instead
+        # (**kwargs) to unpack the dict into keyword arguments for the MenuItem constructor
+        new_menu_item = MenuItem(item_id=str(uuid.uuid4()), **menu_item.model_dump())  #
+        save_all([new_menu_item])
+        return new_menu_item
+
+    def update_menu_item(self, item_id: str, menu_item: MenuItemUpdate):
+        menu_items = load_all()
+        for mi in menu_items:
+            if mi.item_id == item_id:
+                # iterate over updated fields and apply them to the matching menu item
+                for k, v in menu_item.model_dump().items():
+                    setattr(mi, k, v)
+                save_all(menu_items)
+                return mi
+        raise HTTPException(status_code=404, detail="Menu item not found")
+
+    def delete_menu_item(self, item_id: str):
+        menu_items = load_all()
+        for mi in menu_items:
+            if mi.item_id == item_id:
+                menu_items.remove(mi)
+                save_all(menu_items)
+                return
+        raise HTTPException(status_code=404, detail="Menu item not found")
+
+    def get_menu_item(self, user_id: str) -> MenuItem:
+        menu_items = load_all()
+        for mi in menu_items:
+            if mi.item_id == item_id:
+                return mi
+        raise HTTPException(status_code=404, detail="Menu item not found")
