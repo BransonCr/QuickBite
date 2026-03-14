d6275bf89f35ca4c11db0993eadcd59f732acc8a
JulianMohamed12 <julianmmohamed@gmail.com>
2026-03-13 22:32:53 -0700

    changed return results for search to be three fields instead of full restaurant objects

--- app/services/RestaurantSearchService.py
+++ app/services/RestaurantSearchService.py
@@ -6,10 +6,18 @@ async def search_restaurants(query):
     filtered_restaurants = []
     for restaurant in restaurants:
         if query.lower() in restaurant.name.lower():
-            filtered_restaurants.append(restaurant)
+            filtered_restaurants.append(
+                {"restaurant_id": restaurant.restaurant_id,
+                 "name": restaurant.name, 
+                 "location": restaurant.location}
+            )
         else:
             for menuitem in restaurant.menu_list:
                 if query.lower() in menuitem.name.lower():
-                    filtered_restaurants.append(restaurant)
+                    filtered_restaurants.append(
+                        {"restaurant_id": restaurant.restaurant_id,
+                         "name": restaurant.name, 
+                         "location": restaurant.location}
+                    )
                     break
     return filtered_restaurants
