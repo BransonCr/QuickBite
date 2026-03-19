309f0b6b86d0799cc01dd35d574d9b601f91d2a3
JulianMohamed12 <julianmmohamed@gmail.com>
2026-03-19 16:34:44 -0700

    changed router to service import, fixed copy paste error, fixed repeated functionality to have method

--- app/routers/RestaurantSearch.py
+++ app/routers/RestaurantSearch.py
@@ -4,15 +4,15 @@ from app.services.RestaurantSearchService import RestaurantSearchService
 router = APIRouter(
     prefix="/search",
     tags=["search"],
-    responses={404: {"description": "Review not found"}},
+    responses={404: {"description": "Restaurant not found"}},
 )
 
 service = RestaurantSearchService()
 
 @router.get("/{query}")
 async def get_search_restaurants(query: str):
-    return await service.search_restaurants(query)
+    return service.search_restaurants(query)
 
 @router.get("/")
 async def get_all_restaurants():
-    return await service.return_all_restaurants()
+    return service.return_all_restaurants()
--- app/services/RestaurantSearchService.py
+++ app/services/RestaurantSearchService.py
@@ -1,55 +1,54 @@
 from typing import List
-from app.routers.Restaurant import get_all_restaurants
-from app.schemas.RestaurantBrowse import RestaurantBrowseCreate, RestaurantBrowse
+from app.services.Restaurant_service import RestaurantService
+from app.schemas.RestaurantBrowse import RestaurantBrowse, RestaurantBrowse
 
 MAX_PER_PAGE = 10
 
 class RestaurantSearchService:
-    async def search_restaurants(self, query) -> List[List[RestaurantBrowse]]:
-        restaurants = await get_all_restaurants()
+    def __init__(self):
+        self.restaurant_service = RestaurantService()
+
+    def search_restaurants(self, query) -> List[List[RestaurantBrowse]]:
+        restaurants = self.restaurant_service.get_all_restaurants()
         
         curr_page = 0
 
         filtered_restaurants = [[]]
         for restaurant in restaurants:
             if query.lower() in restaurant.name.lower():
-                filtered_restaurant = RestaurantBrowseCreate(
-                    restaurant_id=restaurant.restaurant_id,
-                    name=restaurant.name,
-                    location=restaurant.location
-                )
-                if len(filtered_restaurants[curr_page]) >= MAX_PER_PAGE:
-                    curr_page += 1
+                filtered_restaurant = create_restaurant_broswe(restaurant)
+                curr_page = check_pageination(filtered_restaurants, curr_page)
                 filtered_restaurants[curr_page].append(filtered_restaurant)
             else:
                 for menuitem in restaurant.menu_list:
                     if query.lower() in menuitem.name.lower():
-                        filtered_restaurant = RestaurantBrowseCreate(
-                            restaurant_id=restaurant.restaurant_id,
-                            name=restaurant.name,
-                            location=restaurant.location
-                        )
-                        if len(filtered_restaurants[curr_page]) >= MAX_PER_PAGE:
-                            curr_page += 1
-                            filtered_restaurants.append([])
+                        filtered_restaurant = create_restaurant_broswe(restaurant)
+                        curr_page = check_pageination(filtered_restaurants, curr_page)
                         filtered_restaurants[curr_page].append(filtered_restaurant)
                         break
         return filtered_restaurants
     
-    async def return_all_restaurants(self) -> List[List[RestaurantBrowse]]:
-        restaurants = await get_all_restaurants()
+    def return_all_restaurants(self) -> List[List[RestaurantBrowse]]:
+        restaurants = self.restaurant_service.get_all_restaurants()
         
         curr_page = 0
 
         all_restaurants = [[]]
         for restaurant in restaurants:
-            restaurant_browse = RestaurantBrowseCreate(
-                restaurant_id=restaurant.restaurant_id,
-                name=restaurant.name,
-                location=restaurant.location
-            )
-            if len(all_restaurants[curr_page]) >= MAX_PER_PAGE:
-                curr_page += 1
-                all_restaurants.append([])
+            restaurant_browse = create_restaurant_broswe(restaurant)
+            curr_page = check_pageination(all_restaurants, curr_page)
             all_restaurants[curr_page].append(restaurant_browse)
         return all_restaurants
+    
+def create_restaurant_broswe(restaurant):
+    return RestaurantBrowse(
+        restaurant_id=restaurant.restaurant_id,
+        name=restaurant.name,
+        location=restaurant.location
+    )
+
+def check_pageination(restaurants, curr_page):
+    if len(restaurants[curr_page]) >= MAX_PER_PAGE:
+        curr_page += 1
+        restaurants.append([])
+    return curr_page
