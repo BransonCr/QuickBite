0d7da6b07e206337c65ba09c44d6706055efdd70
JulianMohamed12 <julianmmohamed@gmail.com>
2026-03-20 12:24:22 -0700

    fixed error in declaring the csv paths and small append error

--- app/models/PaymentModel.py
+++ app/models/PaymentModel.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from typing import List, Dict, Any
 
 from app.schemas.Payment import Payment
-DATA_DIR = Path(__file__).resolve().parent/"db"
+DATA_DIR = Path(__file__).parent.parent/"db"
 
 PAYMENT_CSV = DATA_DIR/"payment.csv"
 
@@ -13,7 +13,7 @@ def load_all() ->list[Payment]:
     with open (PAYMENT_CSV,"r") as f:
         reader = csv.DictReader(f)
         for row in reader:
-            payments.append(Payment.model.validate(row))
+            payments.append(Payment.model_validate(row))
     return payments
 
 
