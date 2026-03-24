c7e23ebbfc970c5698ed93a48d2e5ff62d89a300
Leo Arthur Cabral <109047018+Kingking11xD@users.noreply.github.com>
2026-03-23 22:17:40 -0700

    Refactor restaurant creation logic for clarity

--- app/services/Restaurant_service.py
+++ app/services/Restaurant_service.py
@@ -3,29 +3,30 @@ from fastapi import HTTPException
 
 from app.schemas.Restaurant import Restaurant, RestaurantCreate, RestaurantUpdate
 from app.models.RestaurantModel import load_all,save_all
-from app.schemas.MenuItem import MenuItem
+
 class RestaurantService:
     def get_all_restaurants(self):
         return load_all()
     
     def create_restaurant(self,restaurant:RestaurantCreate) ->Restaurant:
         restaurants = load_all()
-        for r in restaurants:
-            if r.name.lower() == restaurant.name.lower() and r.location.lower() == restaurant.location.lower():
-                raise HTTPException(status_code=400, detail="Restaurant with this name and location already exists.")
+        
+        if any(r.name.lower() == restaurant.name.lower() and r.location.lower() == restaurant.location.lower() for r in restaurants):
+            raise HTTPException(status_code=400, detail="Restaurant with this name and location already exists.")
             
         new_restaurant = Restaurant(
-            restaurant_id =str(uuid.uuid4()),
-            owner_id = restaurant.owner_id,
-            name = restaurant.name,
-            location = restaurant.location,
-            postal_code = restaurant.postal_code,
+            restaurant_id=str(uuid.uuid4()),
+            owner_id=restaurant.owner_id,
+            name=restaurant.name,
+            location=restaurant.location,
+            postal_code=restaurant.postal_code,
             contact_info=restaurant.contact_info,
             operating_hours=restaurant.operating_hours,
-            delivery_radius = restaurant.delivery_radius,
-            is_active = True,
-            menu_list = []
+            delivery_radius=restaurant.delivery_radius,
+            is_active=True,
+            menu_list=[]
         )   
+        
         restaurants.append(new_restaurant)
         save_all(restaurants)
         return new_restaurant
