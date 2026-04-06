01aeff88c134278cf24c1daf7d60a85407bc633f
JulianMohamed12 <julianmmohamed@gmail.com>
2026-04-05 23:34:27 -0700

    Revert "added attributes average_price and price_category"
    This reverts commit 10e1dc1be24c39bbd02f470db5618c6da1b7730c.

--- app/schemas/RestaurantBrowse.py
+++ app/schemas/RestaurantBrowse.py
@@ -1,23 +1,16 @@
-from typing import Literal, Optional
 from pydantic import BaseModel
 
 class RestaurantBrowse(BaseModel):
     restaurant_id: str
     name: str
     location: str
-    average_price: float
-    price_category: Optional[Literal["$", "$$", "$$$"]] = None
 
 class RestaurantBrowseCreate(BaseModel):
     restaurant_id: str
     name: str
     location: str
-    average_price: float
-    price_category: Optional[Literal["$", "$$", "$$$"]] = None
 
 class RestaurantBrowseUpdate(BaseModel):
     restaurant_id: str
     name: str
     location: str
-    average_price: float
-    price_category: Optional[Literal["$", "$$", "$$$"]] = None
